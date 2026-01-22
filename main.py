525e5932427afa0b249eae314b464430fc13b721
Gustavo Carneiro <gustavo.carneiro@parceiros.nos.pt>
2026-01-22 12:27:42 -0300

    fix: hotfix 1

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ print("Hello! I'm feature 3")
 print("Hello! I'm feature 4")
 print("I'm a bugfix from release 02")
 print("Hello! I'm the bugfix 01")
-
+print("Hello! I'm a hotfix!") 
 print("Hello! I'm feature 6")
