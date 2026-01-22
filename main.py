40b1c4f1176b800a6c245db8d1c93060ceb58bad
Gustavo Carneiro <gustavo.carneiro@parceiros.nos.pt>
2026-01-22 12:21:02 -0300

    fix: this is a bugfix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("Hello! I'm feauture 2")
 print("Hello! I'm feature 3")
 
 print("Hello! I'm feature 4")
-
+print("I'm a bugfix from release 02")
 print("Hello! I'm the bugfix 01")
 
 print("Hello! I'm feature 6")
